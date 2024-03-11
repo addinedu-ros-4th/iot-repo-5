e6fccd54c66b3954712ab010d0ccc83b10427821
AUTO-KKYU <dknjy3313@gmail.com>
2024-03-11 23:45:49 +0900

    Control buzzer to fix the range of pm10

--- src/arduino_sensor/fixed_ENVsensor/fixed_ENVsensor.ino
+++ src/arduino_sensor/fixed_ENVsensor/fixed_ENVsensor.ino
@@ -9,21 +9,21 @@ DHT dht(DHTPIN, DHTTYPE);
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);  
 
 unsigned long previousMillis = 0;
-const long interval = 5000; // 5 seconds
+const long interval = 1000;
 
 int red = A0;
 int green = A1;
 int blue = A2;
 
-int speakerPin = A5;
-int Vo = A4;
+int speakerPin = A4;
+int Vo = A5;
 int V_LED = 7;  
 
 float Vo_value = 0;
 float Voltage = 0;
 float dustDensity = 0;
 
-bool displayHumidityTemp = true;
+bool displayTempHumi = true; 
 
 void setup() {
   lcd.begin(16, 2);
@@ -47,10 +47,8 @@ void loop() {
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
 
-    float humi, temp;
-    temp = dht.readTemperature();
-    humi = dht.readHumidity();
-
+    float co2, pm10;
+    co2 = sv; 
     digitalWrite(V_LED, LOW);
     delayMicroseconds(280);
     Vo_value = analogRead(Vo);
@@ -58,36 +56,70 @@ void loop() {
     digitalWrite(V_LED, HIGH);
     delayMicroseconds(9680);
 
-    Voltage = Vo_value * 5.0 / 1023.0;
-    dustDensity = (Voltage - 0.3) / 0.005;
+    Voltage = Vo_value * 5.0 / 1024.0;
+    dustDensity = (Voltage - 0.1) / 0.005;
 
-    if (isnan(humi) || isnan(temp)) {
-      Serial.println("Failed to read from DHT Sensor!!");
-      return;
-    }
-    
-    if (displayHumidityTemp) {
+    if (displayTempHumi) {
+      float temp, humi;
+      temp = dht.readTemperature();
+      humi = dht.readHumidity();
+
+      if (isnan(humi) || isnan(temp)) {
+        Serial.println("Failed to read from DHT Sensor!!");
+        return;
+      }
+
+      Serial.print("Temperature: ");
+      Serial.print(temp);
+      Serial.print("°C , Humidity: ");
+      Serial.print(humi);
+      Serial.print("%, CO2: ");
+      Serial.print(sv);
+      Serial.print("ppm , PM10: ");
+      Serial.print(dustDensity);
+      Serial.println(" ug/m3");
+      
+      lcd.clear();
+      
       lcd.setCursor(0, 0);
-      lcd.print("Humi: "); lcd.print(humi); lcd.print(" %");
+      lcd.print("Temp:"); lcd.print(temp); lcd.print(" C");
+      
       lcd.setCursor(0, 1);
-      lcd.print("Temp: "); lcd.print(temp); lcd.print(" C");
+      lcd.print("Humi:"); lcd.print(humi); lcd.print(" %");
     } else {
+      lcd.clear(); 
+      
       lcd.setCursor(0, 0);
-      lcd.print("CO2: "); lcd.print(sv); lcd.print(" ppm");
+      lcd.print("CO2:"); lcd.print(co2); lcd.print(" PPM");
+      
       lcd.setCursor(0, 1);
-      lcd.print("PM10: "); lcd.print(dustDensity); lcd.print(" ug/m3");
+      lcd.print("PM10:"); lcd.print(dustDensity); lcd.print(" ug/m3");
     }
 
-    Serial.print("Temperature: ");
-    Serial.print(temp);
-    Serial.print("°C , Humidity: ");
-    Serial.print(humi);
-    Serial.print("%, CO2: ");
-    Serial.print(sv);
-    Serial.print("ppm , PM10: ");
-    Serial.print(dustDensity);
-    Serial.println(" ug/m3");
+    displayTempHumi = !displayTempHumi; 
 
-    displayHumidityTemp = !displayHumidityTemp; // Toggle the display state
+    if (dustDensity <= 50.0) {
+      analogWrite(red, 0);
+      analogWrite(green, 255);
+      analogWrite(blue, 0);
+
+      noTone(speakerPin);
+    } 
+    else if (dustDensity <= 300.0) {
+      analogWrite(red, 0);
+      analogWrite(green, 0);
+      analogWrite(blue, 255);
+
+      noTone(speakerPin);
+    }
+    else {
+      analogWrite(red, 255);
+      analogWrite(green, 0);
+      analogWrite(blue, 0);
+
+      tone(speakerPin, 1000); 
+      delay(500); 
+      noTone(speakerPin); 
+    }
   }
 }
