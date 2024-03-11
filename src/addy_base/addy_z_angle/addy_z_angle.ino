8315b511743ff312375b512f3e3ab9543bfa8a88
YoonHJ97 <xxbb96@gmail.com>
2024-03-12 02:33:14 +0900

    modify z_angle

--- src/addy_base/addy_z_angle/addy_z_angle.ino
+++ src/addy_base/addy_z_angle/addy_z_angle.ino
@@ -1,5 +1,9 @@
 #include <Wire.h>
 
+unsigned long lastResetTime = 0;  // 마지막 초기화 시간
+unsigned long resetInterval = 10000;  // 초기화 간격 (10초
+float angle[3] = {0,}, vec;
+
 void setup() {
   // Serial init
   Serial.begin(9600);
@@ -11,8 +15,7 @@ void setup() {
   Wire.write(0);
   Wire.endTransmission();
   // Register 26
-  for(uint8_t i = 2; i <= 7; i++)
-  {
+  for (uint8_t i = 2; i <= 7; i++) {
     Wire.beginTransmission(0x68);
     Wire.write(26);
     Wire.write(i << 3 | 0x03);
@@ -29,18 +32,19 @@ void setup() {
   Wire.write(0);
   Wire.endTransmission();
 }
+
 int16_t offset[3] = {-22, 15, -12};
 
 int get_Z() {
   uint8_t i;
-  static int16_t gyro_raw[3]={0,};
+  static int16_t gyro_raw[3] = {0,};
 
   // Get Gyro
   Wire.beginTransmission(0x68);
   Wire.write(67);
   Wire.endTransmission();
   Wire.requestFrom(0x68, 6);
-  for(i = 0; i < 3; i++)
+  for (i = 0; i < 3; i++)
     gyro_raw[i] = gyro_raw[i] * 0.8 + 0.2 * (((Wire.read() << 8) | Wire.read()) - offset[i]);
   // Get DT
   static unsigned long p = 0;
@@ -49,19 +53,27 @@ int get_Z() {
   p = c;
   // Gyro Rate
   float gyro_rate[3];
-  for(i = 0; i < 3; i++) gyro_rate[i] = gyro_raw[i] / 16.4 * dt;
+  for (i = 0; i < 3; i++) gyro_rate[i] = gyro_raw[i] / 16.4 * dt;
   // Calculate
-  static float angle[3]={0,}, vec;
 
   // Serial print
   angle[2] += gyro_rate[2];
   char ang_z[10];
 
   dtostrf(angle[2], 4, 3, ang_z);
-  
 
   return atoi(ang_z);
 }
+
+void resetAngle() {
+  Serial.println("Resetting Angle!");
+  for (int i = 0; i < 3; i++) {
+    angle[i] = 0;
+  }
+}
+
 void loop() {
+
+
   Serial.println(get_Z());
 }
